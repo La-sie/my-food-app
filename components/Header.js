8ce9c13eb7746d4bc160d092890ebf2642183c03
Sellasie <sellasie2019@gmail.com>
2024-06-12 17:44:03 +0000

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import {View, StyleSheet, Text} from 'react-native';
+import Logo from './Logo';
+
+
+export default function Header(){
+  return(
+    <View style={styles.container}>
+      <View style={styles.logotxt}>
+       <Logo/>
+       <Text style={{marginLeft: 15, fontWeight: 'bold', fontSize: 19}}> Message </Text>
+      </View>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    backgroundColor: '#fff'
+  },
+  logotxt:{
+    flexDirection: 'row',
+    alignItems: 'center', 
+    marginTop: 5, 
+    marginLeft: 15,
+  }
+})
